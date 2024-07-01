a1c29837477de309a6bd5b7738e69a68b584e4ba
Matheus <matheus.limamst@gmail.com>
2024-07-01 17:06:31 -0300

    update area

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const areasItems = document.querySelectorAll('.areas__item');
+  areasItems.forEach(item => {
+    item.classList.add('hidden');
+  });
+});
+
 const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 const mobileLinks = document.querySelectorAll('.mobile__links a');
+const toggleButton = document.querySelector('#toggleAreas');
+const areasContent = document.querySelector('.areas__content');
+const areasItems = document.querySelectorAll('.areas__item');
+
+toggleButton.addEventListener('click', function() {
+  areasContent.classList.toggle('active');
+  toggleButton.classList.toggle('active');
+  areasItems.forEach(item => {
+    item.classList.toggle('hidden');
+  });
+});
 
 button.addEventListener('click', function () {
   mobileNavbar.classList.toggle('active');
@@ -31,7 +49,9 @@ function isVisible(element) {
 }
 
 window.addEventListener('scroll', function(){
-    if(this.window.pageYOffset >0) return navbar.classList.add('active')
-        return navbar.classList.remove('active');
+  if(this.window.pageYOffset > 0) {
+    navbar.classList.add('active');
+  } else {
+    navbar.classList.remove('active');
+  }
 });
-
